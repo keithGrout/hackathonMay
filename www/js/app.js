6491f380f0705145d05e8271672cb618ac521504
keithGrout <keiwi.g@gmail.com>
2014-05-19 12:37:52 -0700

    added repeating tasks

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,10 @@
-angular.module('gyst', ['ionic']);
+angular.module('GYST', ['ionic'])
+
+.controller('GystCtrl', function($scope) {
+ $scope.tasks = [
+   { title: 'Collect coins' },
+   { title: 'Eat mushrooms' },
+   { title: 'Get high enough to grab the flag' },
+   { title: 'Find the Princess' }
+ ];
+});
